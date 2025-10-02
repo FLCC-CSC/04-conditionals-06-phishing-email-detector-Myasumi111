1833aae6206d6abf7500b5f78e9d2c93bc5e80a3
Myasumi111 <myasumi@fingerlakes.edu>
2025-10-02 14:34:30 -0400

    Update phishing_email_detector.py

--- phishing_email_detector.py
+++ phishing_email_detector.py
@@ -20,21 +20,18 @@ def main():
 
 def detecter():
     subject = input("Enter the email subject line: ")
-    print("SECURITY ASSESSMENT: ")
     print()
-    if "urgent" in subject.lower():
+    print("SECURITY ASSESSMENT: ")
+
+    if "urgent" in subject or "immediate action required" in subject :
         print("HIGH RISK: Possible phishing attempt.")
-    elif "immediate action required" in subject.lower():
-        print("HIGH RISK: Possible phishing attempt.")
-    elif "win" in subject.lower():
+    elif "win" in subject or "free" in subject :
         print("MEDIUM RISK: Suspicious offer detected.")
-    elif "free" in subject.lower() :
-        print("MEDIUM RISK: Suspicious offer detected.")
-    elif "password reset" in subject.lower():
+    elif "password reset" in subject:
         print("LOW RISK: Verify legitimacy with sender.")
     else:
         print("No phishing indicators detected.")
-    print("------------------------")
+
     print(f'Analyzed subject: "{subject}"')
 
 main()
